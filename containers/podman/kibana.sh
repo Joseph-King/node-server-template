701bdd2d54b4ab040e3a229ecee28e3f5073296f
Joseph King <joseph.king@associates.cisa.dhs.gov>
2025-12-01 13:51:42 -0500

    Initial database connections working

--- containers/podman/kibana.sh
+++
@@ -1,7 +0,0 @@
-podman network create dev
-
-podman run -d --network dev --name kibana \
-    -p 5601:5601 \
-    -e ELASTICSEARCH_PASSWORD=kibana \
-    -e ELASTICSEARCH_USERNAME=kibana_user \
-    docker.elastic.co/kibana/kibana:8.15.2
